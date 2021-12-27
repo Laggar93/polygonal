57b5fbaa667aaf63d6ddf5309daff580ff5073c7
Laggar <ulikfr1d@gmail.com>
2021-12-27 16:23:32 +0600

    delete diff from catalog, remake no-items page and finished category button styles

--- shop/views.py
+++ shop/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 from .service import get_order_params
 
 
-
 def category_view(request, category_slug):
     query_param = request.GET.get('sort')
     get_object_or_404(category, slug=category_slug)
@@ -14,7 +13,15 @@ def category_view(request, category_slug):
         'category').order_by('name')
 
     if not items:
-        return render(request, 'no_items.html')
+        categories = category.objects.all()
+        subcategories = subcategory.objects.filter(category__slug=category_slug)
+        active_category = category.objects.filter(slug=category_slug).first()
+        context = {
+            'categories': categories,
+            'subcategories': subcategories,
+            'active_category': active_category
+        }
+        return render(request, 'no_items.html', context=context)
 
     if query_param and query_param != None:
         param = get_order_params(query_param)
@@ -37,28 +44,35 @@ def category_view(request, category_slug):
     return render(request, 'catalog.html', context=context)
 
 
-
 def first_category_for_url(request):
-
     active_category = category.objects.all().first()
     path = active_category.slug
 
     return redirect(category_view, category_slug=path)
 
 
-
 def subcategory_view(request, category_slug, subcategory_slug):
     query_param = request.GET.get('sort')
     subcategory_slug = subcategory_slug.split(' ')
     get_object_or_404(category, slug=category_slug)
-    get_object_or_404(subcategory, slug=subcategory_slug[0], category=category.objects.filter(slug=category_slug).first())
+    get_object_or_404(subcategory, slug=subcategory_slug[0],
+                      category=category.objects.filter(
+                          slug=category_slug).first())
 
     items = item.objects.filter(category__category__slug=category_slug,
                                 category__slug=subcategory_slug[0]).order_by(
         'name')
 
     if not items:
-        return render(request, 'no_items.html', )
+        categories = category.objects.all()
+        subcategories = subcategory.objects.filter(category__slug=category_slug)
+        active_category = category.objects.filter(slug=category_slug).first()
+        context = {
+            'categories': categories,
+            'subcategories': subcategories,
+            'active_category': active_category
+        }
+        return render(request, 'no_items.html', context=context)
 
     if query_param:
         param = get_order_params(query_param)
