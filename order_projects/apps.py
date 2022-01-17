8fce0e7c7e05a523a61132c2484c7114e973c7f5
Laggar <ulikfr1d@gmail.com>
2022-01-17 20:17:48 +0600

    change verbode name in apps.py file

--- order_projects/apps.py
+++ order_projects/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class OrderProjectsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'order_projects'
+    verbose_name = 'Проекты на заказ'
