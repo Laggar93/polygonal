31fb7a445cc35970fff4a2b82dae08e71c988918
Laggar <ulikfr1d@gmail.com>
2022-01-18 14:40:37 +0600

    add two charters in view

--- order_projects/views.py
+++ order_projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.shortcuts import render
 from .models import project_page, project_images, project_list
+from shop.models import shop_page
 
 
 def projects_view(request):
@@ -9,6 +10,7 @@ def projects_view(request):
 
     context = {
         'show_language': True,
+        'shop_page': shop_page.objects.first(),
     }
 
     return render(request, 'projects.html', context=context)
