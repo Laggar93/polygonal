6e456cc6f6551cf875efb828060a57fbd53616d9
Laggar <ulikfr1d@gmail.com>
2022-01-18 15:08:38 +0600

    add language switch to projects page

--- order_projects/views.py
+++ order_projects/views.py
@@ -7,10 +7,16 @@ from shop.models import shop_page
 def projects_view(request):
     # get_object_or_404(project_page)
     # projects = project_page.objects.all()
+    link_ru = '/ru/projects/'
+    link_en = '/en/projects/'
+    link_fr = '/fr/projects/'
 
     context = {
         'show_language': True,
         'shop_page': shop_page.objects.first(),
+        'link_en': link_en,
+        'link_fr': link_fr,
+        'link_ru': link_ru,
     }
 
     return render(request, 'projects.html', context=context)
